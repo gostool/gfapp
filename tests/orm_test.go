b42b0075bf93576af0ec200a0ea3653a0149e3cf
huoyinghui <yhhuo@yunjinginc.com>
2021-05-19 18:39:21 +0800

    update: orm curd

--- tests/orm_test.go
+++ tests/orm_test.go
@@ -14,3 +14,33 @@ func TestInsert(t *testing.T) {
 	}
 }
 
+
+// Update
+func TestUpdate(t *testing.T) {
+	// UPDATE `user` SET `name`='john guo' WHERE name='john'
+	_, err := g.DB().Table("user").Data("name", "john guo").
+		Where("name", "john").Update()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Delete
+func TestDelete(t *testing.T) {
+	// DELETE FROM `user` WHERE uid=10
+	_, err := g.DB().Table("user").Where("id", 4).Delete()
+	if err != nil {
+		panic(err)
+	}
+}
+
+
+// Select Where
+func TestWhere(t *testing.T) {
+	// 数量
+	count, err := g.DB().Table("user").Where("id", 3).Count()
+	if err != nil {
+		panic(err)
+	}
+	g.Log().Infof("user id=3, count:%v", count)
+}
