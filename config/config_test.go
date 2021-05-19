53eb04aedc8cc5573b2f246ebec7f48a762a4b94
huoyinghui <yhhuo@yunjinginc.com>
2021-05-19 17:54:25 +0800

    add: config test

--- config/config_test.go
+++ config/config_test.go
@@ -8,4 +8,14 @@ import (
 func TestCfg(t *testing.T) {
 	address := g.Config().Get("server.Address")
 	t.Logf("Address:%v\n", address)
+	g.Log("abc")
+}
+
+func TestCfgSetPath(t *testing.T) {
+	g.Cfg().SetFileName("config_tpl.toml")
+	address := g.Config().Get("server.Address")
+	if address.(string) != ":8299" {
+		t.Logf("addr(%v) != :8299 in config_tpl.toml", address)
+		t.Fail()
+	}
 }
