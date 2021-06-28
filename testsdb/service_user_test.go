60cb1d55a2bd88c84d1b5597255a7570f8e99e90
tphyhFighting <2363176358@qq.com>
2021-06-28 16:05:06 +0800

    update: service user test

--- testsdb/service_user_test.go
+++ testsdb/service_user_test.go
@@ -6,22 +6,22 @@ import (
 	"testing"
 )
 
-var testUserServiceReq model.UserRegisterServiceSignUpReq
-var testUserServiceMailReq model.UserRegisterServiceSignUpReq
-var testUserServicePhoneReq model.UserRegisterServiceSignUpReq
+var testUserServiceReq model.UserRegisterServiceReq
+var testUserServiceMailReq model.UserRegisterServiceReq
+var testUserServicePhoneReq model.UserRegisterServiceReq
 
 func init() {
-	testUserServiceReq = model.UserRegisterServiceSignUpReq{
+	testUserServiceReq = model.UserRegisterServiceReq{
 		Password:     "123456",
 		Passport:     "admin04",
 		RegisterType: 1,
 	}
-	testUserServiceMailReq = model.UserRegisterServiceSignUpReq{
+	testUserServiceMailReq = model.UserRegisterServiceReq{
 		Email:        "1353176359@qq.com",
 		Password:     "123456",
 		RegisterType: 2,
 	}
-	testUserServicePhoneReq = model.UserRegisterServiceSignUpReq{
+	testUserServicePhoneReq = model.UserRegisterServiceReq{
 		Phone:        "17792301520",
 		Password:     "123456",
 		RegisterType: 3,
@@ -33,9 +33,11 @@ func TestServiceRegister(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = service.User.Register(&testUserServiceReq)
+	id, err := service.User.RegisterAccount(&testUserServiceReq)
 	if err != nil {
 		t.Fatal(err)
+	} else {
+		t.Logf("ID:%v\n", id)
 	}
 }
 
@@ -44,9 +46,11 @@ func TestServiceMailRegister(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = service.User.Register(&testUserServiceMailReq)
+	id, err := service.User.RegisterEmail(&testUserServiceMailReq)
 	if err != nil {
 		t.Fatal(err)
+	} else {
+		t.Logf("ID:%v\n", id)
 	}
 }
 
@@ -55,9 +59,11 @@ func TestServicePhoneRegister(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = service.User.Register(&testUserServicePhoneReq)
+	id, err := service.User.RegisterPhone(&testUserServicePhoneReq)
 	if err != nil {
 		t.Fatal(err)
+	} else {
+		t.Logf("ID:%v\n", id)
 	}
 }
 
